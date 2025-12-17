348db2f45f425c4c1511ddbc9ec17db00f4e9933
thalesbxempresa <thalesbxempresa@gmail.com>
2025-12-17 03:45:18 -0300

    Hide product price if 0 or null

--- src/components/AdminProducts.jsx
+++ src/components/AdminProducts.jsx
@@ -174,7 +174,7 @@ const AdminProducts = () => {
                             <p style={{ fontSize: '0.9rem', opacity: 0.7, marginBottom: '0.5rem', display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', overflow: 'hidden' }}>
                                 {product.description}
                             </p>
-                            {product.price && (
+                            {product.price > 0 && (
                                 <p className="text-gradient" style={{ fontWeight: 'bold', fontSize: '1.1rem' }}>
                                     R$ {parseFloat(product.price).toFixed(2).replace('.', ',')}
                                 </p>
